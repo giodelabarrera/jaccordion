9816aefa56b7af9fa63c1b5743fc873dd6564324
Giorgio de la Barrera <giodelabarrera@gmail.com>
2019-02-14 19:56:14 +0100

    Add default config

---
+++ src/defaults.js
@@ -0,0 +1,9 @@
+export default {
+  multiOpen: false,
+  allowAllClosed: false,
+  classes: {
+    header: 'jaccordion__header',
+    isOpened: 'jaccordion__header--isOpened',
+    content: 'jaccordion__content'
+  }
+}
