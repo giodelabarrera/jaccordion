315b0027f517f788f2045f5dd0d8a5d7c0e7f232
Giorgio de la Barrera <giodelabarrera@gmail.com>
2019-02-14 19:44:47 +0100

    Move Jaccordion from index

---
+++ src/Jaccordion.js
@@ -0,0 +1,43 @@
+import {makeIsTagName} from './utils/dom'
+import Item from './Item'
+
+const isDTTagName = makeIsTagName('dt')
+
+export default class Jaccordion {
+  constructor(element) {
+    this.element = element
+
+    this.items = []
+  }
+
+  mount() {
+    const children = Array.from(this.element.children)
+
+    this.items = children.filter(isDTTagName).map(header => {
+      const content = header.nextElementSibling
+      return new Item({header, content})
+    })
+
+    return this
+  }
+
+  update(settings) {
+    return this
+  }
+
+  destroy() {
+    return this
+  }
+
+  disable() {
+    return this
+  }
+
+  enable() {
+    return this
+  }
+
+  on(event, handler) {
+    return this
+  }
+}
