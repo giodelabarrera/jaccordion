bced7cde3018520869366b95e6e99d0b869df96a
Gio De la Barrera <giodelabarrera@gmail.com>
2019-03-12 15:05:23 +0100

    Add doc for core entry

--- src/core/entry.js
+++ src/core/entry.js
@@ -3,6 +3,17 @@ import {throwRequiredError, throwTypeError} from '../utils/throw-error'
 import {callApi} from '../utils/service'
 import {validateEntry} from './validator'
 
+/**
+ * Gets the entries processed by the ajax
+ * @param {Object} ajax - The ajax object
+ * @param {String} ajax.url - The url to make the request
+ * @param {Function} ajax.processResults - The processResults function to the results of the request
+ * @throws {Error} Call api
+ * @throws {Error} Required entries of processResults
+ * @throws {Error} Incorrect type of entries of processResults
+ * @throws {Error} Validation of entries
+ * @returns {Promise<Array>} Entries processed by processResults function
+ */
 export async function getEntriesByAjax(ajax) {
   const {url, processResults} = ajax
   const data = await callApi(url)
